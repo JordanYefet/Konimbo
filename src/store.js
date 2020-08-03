fec1f6331b68004edc2a41bcf850f18c477c90fa
Jordan Yefet <Jordan.Yefet90@gmail.com>
2020-08-03 21:06:14 +0300

    API censorate

--- src/store.js
+++ src/store.js
@@ -23,9 +23,7 @@ export const store = new Vuex.Store({
     fetchItems(context) {
       context.commit("SET_LOADING_STATUS", "loading");
       axios
-        .get(
-          "https://api.konimbo.co.il/v1/items?token=9c1a92bf8cefc59e4ec9fa7c53bba0f90dd8b15850bef1062dbf32c5e8fd3a08"
-        )
+        .get("<PUT API HERE>")
         .then((response) => {
           context.commit("SET_LOADING_STATUS", "notLoading");
           context.commit("SET_ITEMS", response.data);
